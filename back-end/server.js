8dd0a45d49a0212c39ffa408ba5733931b57e24f
Steven Rummler <steven.rummler@gmail.com>
2020-11-20 15:06:22 -0700

    changed upload destination path

--- back-end/server.js
+++ back-end/server.js
@@ -29,7 +29,7 @@ const Item = mongoose.model("Item", itemSchema);
 // Configure multer so that it will upload to '/var/www/lab4.stevenrummler.com/images/'
 const multer = require("multer");
 const upload = multer({
-  dest: "/var/www/lab4.stevenrummler.com/images/",
+  dest: "images/",
   limits: {
     fileSize: 10000000,
   },
